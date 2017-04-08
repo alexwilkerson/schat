3cd50b1fd0ad7e1aee58ab529953df8b9d670924
Alex Wilkerson <alex@uncorrected.com>
2017-04-08 02:44:43 -0500

    completely overhauled all of the bash scripts and converted everything to two python scripts. the bottom script is called feeder. the top script is called eater.

--- Makefile
+++ Makefile
@@ -1,16 +1,18 @@
 update:
 	sed -e 's#dir="./"#dir="/tmp/"#' schat.sh > /tmp/.schat.sh
 	chmod a+x /tmp/.schat.sh
-	cp .commander /tmp/.commander
+	cp feeder /tmp/.feeder
+	chmod a+x /tmp/.feeder
 	cp eater /tmp/.eater
 	chmod a+x /tmp/.eater
 	touch /tmp/.schat.log
-	chmod a+wr /tmp/.schat.log
+	chmod a+rw /tmp/.schat.log
+	cp termcolors.py /tmp/termcolors.py
+	chmod a+rw /tmp/termcolors.py
 
 purify:
 	rm /tmp/.schat.sh
-	rm /tmp/.commander
 	rm /tmp/.eater
-	rm /tmp/.top_pane_awilkers.sh
-	rm /tmp/.bottom_pane_awilkers.sh
+	rm /tmp/.feeder
+	rm /tmp/termcolors.py
 	rm /tmp/.schat.log
--- schat.sh
+++ schat.sh
@@ -6,68 +6,13 @@
 # Usage: ./schat.sh
 ############################
 
-# get username
-userid=$(whoami)
-# change this to /tmp/ if updating
-dir="./"
-
-# script for bottom pane
-cat > /tmp/.bottom_pane_$userid.sh << EOF
-#!$(which bash)
-function cleanup {
-    rm /tmp/.bottom_pane_$userid.sh
-    tmux kill-session -t schat
-}
-
-echo -e "[\$(TZ=UTC+5 date '+%F %H:%M')] \e[1m\e[35m$userid enters the room.\e[0m" >> \\
-    /tmp/.schat.log
-
-while true; do
-    echo -en "\r\e[1m\e[35mmessage: \e[0m"
-    read -e messg
-    if [ "\$messg" == '!exit' ]
-    then
-        echo -e "[\$(TZ=UTC+5 date '+%F %H:%M')] \e[1m\e[35m$userid leaves the room.\e[0m" >> \\
-        /tmp/.schat.log
-        break
-    elif [ "\$messg" == '!deletelog' ]
-    then
-        rm /tmp/.schat.log
-        touch /tmp/.schat.log
-        chmod a+w /tmp/.schat.log
-        echo -e "[\$(TZ=UTC+5 date '+%F %H:%M')] \e[1m\e[35m$userid deleted chat log.\e[0m" >> \\
-        /tmp/.schat.log
-        clear
-    elif [ "\$messg" == 'Shrek' ]
-    then
-        while read -e line; do
-            sleep 1
-            echo -e "[\$(TZ=UTC+5 date '+%F %H:%M')] \e[1m\e[34m$userid:\e[0m \$line" >> \\
-            /tmp/.schat.log && \\
-            clear
-        done < /tmp/.allstar.txt
-    elif [[ \$messg =~ ^! ]]
-    then
-        clear
-        setterm -cursor off
-        $dir.commander $userid "\$messg"
-        setterm -cursor on
-        clear #comment out this line to error check
-    else
-        echo -e "[\$(TZ=UTC+5 date '+%F %H:%M')] \e[1m\e[34m$userid:\e[0m \$messg" >> \\
-        /tmp/.schat.log && \\
-        clear
-    fi
-done
-cleanup
-EOF
-
-chmod +x /tmp/.bottom_pane_$userid.sh
-chmod +x /tmp/.eater
-chmod a+w /tmp/.schat.log
+chmod a+x /tmp/.eater
+chmod a+x /tmp/.feeder
+chmod a+r /tmp/termcolors.py
+chmod a+rw /tmp/.schat.log
 
 # set up tmux session
 tmux new-session -d -s "schattr" "/tmp/.eater"
-tmux split-window -v "/tmp/.bottom_pane_$userid.sh"
+tmux split-window -v "/tmp/.feeder"
 tmux resize-pane -D 20
 tmux attach-session 
