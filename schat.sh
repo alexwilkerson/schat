5864d89e154fe8620b4b3be76d2214ec2efa031e
Alex Wilkerson <alex@uncorrected.com>
2017-04-10 02:30:37 -0500

    before starting server

--- schat.sh
+++ schat.sh
@@ -12,10 +12,10 @@ chmod a+r /tmp/termcolors.py
 chmod a+rw /tmp/.schat.log
 
 # set up tmux session
-tmux kill-session -t "schattr"
+# tmux kill-session -t "schattr"
 tmux new-session -d -s "schattr" "/tmp/.eater"
 tmux split-window -h "/tmp/.dxbdxb7"
 tmux resize-pane -t 1 -x 15
 tmux split-window -t 0 -v "/tmp/.feeder"
 tmux resize-pane -t 2 -y 2
-tmux attach-session 
+tmux attach-session
